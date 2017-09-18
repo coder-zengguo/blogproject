d64a37981b3bdf99b0a98145bca5a232e5188567
yediyeti <yediyeti@163.com>
2017-09-18 16:38:22 +0800

    Step5: blog index view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
+#encoding=utf-8
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(requset):
+    return render(requset,'blog/index.html',context={
+        'title': '小果子的博客首页',
+        'welcome': '欢迎访问小果子的博客首页'
+    })
