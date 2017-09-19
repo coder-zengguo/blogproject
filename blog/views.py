6e413182968f53e4412dcaeaf0bec5f079de5d00
yediyeti <yediyeti@163.com>
2017-09-19 09:24:34 +0800

    Step6: real blog index view

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,9 @@
 #encoding=utf-8
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
-def index(requset):
-    return render(requset,'blog/index.html',context={
-        'title': '小果子的博客首页',
-        'welcome': '欢迎访问小果子的博客首页'
-    })
+def index(request):
+    post_list = Post.objects.all().order_by('-created_time')
+    return render(request,'blog/index.html',context={'post_list': post_list})
