671a15956da4c522c13f414d9977d5213814cc60
yediyeti <yediyeti@163.com>
2017-09-19 16:52:35 +0800

    Step9-markdown-and-code-highlight-supported

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
 #encoding=utf-8
+import markdown
+import pygments
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Post
+
 
 # Create your views here.
 def index(request):
@@ -10,4 +13,11 @@ def index(request):
 
 def detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
+    # Markdown 渲染
+    post.body = markdown.markdown(post.body,
+                                extensions=[
+                                    'markdown.extensions.extra',
+                                    'markdown.extensions.codehilite',
+                                    'markdown.extensions.toc',
+                                  ])
     return render(request, 'blog/detail.html', context={'post': post})
