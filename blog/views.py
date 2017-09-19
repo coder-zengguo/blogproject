580f9cd0433f7e48d145e8be29d9a9e6aff2b3c6
yediyeti <yediyeti@163.com>
2017-09-19 20:09:38 +0800

    Step11: category and archive

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ import markdown
 import pygments
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from .models import Post
+from .models import Post, Category
 
 
 # Create your views here.
@@ -21,3 +21,15 @@ def detail(request, pk):
                                     'markdown.extensions.toc',
                                   ])
     return render(request, 'blog/detail.html', context={'post': post})
+
+def archives(request, year, month):
+    post_list = Post.objects.filter(created_time__year=year,
+                                    created_time__month=month
+                                    ).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
+
+def category(request, pk):
+    # 记得在开始部分导入 Category 类
+    cate = get_object_or_404(Category, pk=pk)
+    post_list = Post.objects.filter(category=cate).order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list': post_list})
