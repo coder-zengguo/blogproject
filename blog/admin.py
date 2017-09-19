2d3432b3d2aa7fced634b3cd23add5f5cd40737d
yediyeti <yediyeti@163.com>
2017-09-19 10:25:20 +0800

    Step7: publish posts using django admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,13 @@
+#encoding=utf-8
 from django.contrib import admin
+from .models import Post, Category, Tag
 
+# 定制admin后台
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'created_time', 'modified_time', 'category', 'author']
+    
 # Register your models here.
+# 将新增的PostAdmin的注册
+admin.site.register(Post,PostAdmin)
+admin.site.register(Category)
+admin.site.register(Tag)
