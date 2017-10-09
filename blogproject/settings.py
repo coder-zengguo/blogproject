325e2ed2cc2cb28f1e2fc82037702b38a2929aec
yediyeti <yediyeti@163.com>
2017-10-09 23:46:55 +0800

    使用 CDN 加速静态资源的加载。

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -123,5 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-# # add configure
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# add configure
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
